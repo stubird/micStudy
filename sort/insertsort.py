d4a24e6af2c055dfee49d94233b450f1d4f2ed6c
stubird <falcon_dt@yeah.net>
2018-01-16 19:26:34 +0800

    three insert sort

--- sort/insertsort.py
+++ sort/insertsort.py
@@ -20,14 +20,37 @@ Simple Insert sort
 from random import random
 from time import time
 
-length = 10000
-arraytoSort = [ int(random()*100) for _ in range(length)]
+length = 5000
+arraytoSort = [ int(random()*1000) for _ in range(length)]
+orgin = arraytoSort[:]
 
+#because big array item swap operation, the algorithm is not efficient in python
+timepoint = time()
+count = 0
+for x in range(1, len(arraytoSort)):
+    for i in range(x-1,-1,-1):
+        count = count + 1
+        if arraytoSort[i] > arraytoSort[i+1]:
+            arraytoSort[i],arraytoSort[i+1] = arraytoSort[i+1],arraytoSort[i]
+        else:
+            break
+
+timepoint =  time() - timepoint
+
+#print("orgin",arraytoSort)
+#print("sorted",orgin)
+print('time spend ',timepoint,' loop times', count)
+
+#use orgin array
+arraytoSort = orgin[:]
 result = [arraytoSort.pop()]
 
+# better efficient but use a middle array which means more space used
 timepoint = time()
+count = 0
 for x in arraytoSort:
     for y in range(len(result)):
+        count = count + 1
         if x <= result[y]:
             newr = result[0:y]
             newr.extend([x])
@@ -41,4 +64,36 @@ timepoint =  time() - timepoint
 
 # print("orgin",arraytoSort)
 # print("sorted",result)
-print(timepoint)
+print('time spend ',timepoint,' loop times', count)
+
+#use orgin array
+arraytoSort = orgin[:]
+
+# more efficient
+timepoint = time()
+count = 0
+for x in range(1, len(arraytoSort)):
+    right = x
+    left = x
+    sign = arraytoSort[x]
+    for i in range(x-1,-1,-1):
+        count = count + 1
+        if arraytoSort[i] < sign:
+            left = i
+        else:
+            break
+
+    first = arraytoSort[:left]
+    middle = arraytoSort[left:right]
+    end = arraytoSort[right+1:]
+
+    first.extend([sign])
+    first.extend(middle)
+    first.extend(end)
+    arraytoSort = first
+
+timepoint =  time() - timepoint
+
+#print("orgin",orgin)
+#print("sorted",arraytoSort)
+print('time spend ',timepoint,' loop times', count)
